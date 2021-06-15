caf45e3ca39347a2d7341707ed97bae47135c645
Lalitha Soumya Ayyagari <soumyaayyagari2310@gmail.com>
2021-06-15 10:04:11 +0530

    Changes made to Page

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get("/", function(req, res){
     let i = __dirname + "/frontend/html/home.html";
     res.sendFile(i);
 });
+
+app.get("/login", function(req, res){
+    let i = __dirname + "/frontend/html/login.html";
+    res.sendFile(i);
+});
+
+app.get("/register", function(req, res){
+    let i = __dirname + "/frontend/html/register.html";
+    res.sendFile(i);
+});
  
 app.listen(PORT, function(){
     console.log("Server Starting running on http://localhost:"+PORT);
