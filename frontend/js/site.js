1d8243b9c1948014f7ebaf165fe44c4843e93432
Lalitha Soumya Ayyagari <soumyaayyagari2310@gmail.com>
2021-06-24 10:31:32 +0530

    Some changes made to Payment page

--- frontend/js/site.js
+++ frontend/js/site.js
@@ -13,6 +13,58 @@ function logout() {
     })
 }
 
+function pageInitialise(){
+    var userObject = {
+        saveUserInLocalStorage : function(userJson){
+            window.localStorage.setItem('currentUser', JSON.stringify(userJson));
+        },
+        removeCurrentUser: function(){
+            window.localStorage.removeItem('currentUser');
+        },
+        getCurrentUser : function(){
+            return window.localStorage.getItem('currentUser');
+        },
+        getCurrentUserName : function(){
+            var curUserString = this.getCurrentUser();
+            if(curUserString){
+                var json = JSON.parse(curUserString);
+                if(json && json.username)
+                    return json.username;
+                return "";
+            }
+            return "";
+        },
+        isUserLoggedIn : function(){
+            if(this.getCurrentUser()==null)
+                return false;
+            return true;
+        }
+    };
+
+    var onSignIn = function(loggedIn){
+        if(loggedIn){
+            console.log("Logged In");
+            $("#signedIn").show();
+            $("#notSignedIn").hide();
+
+        }
+        else{
+            console.log("Not Logged In");
+            $("#notSignedIn").show();
+            $("#signedIn").hide();
+        }
+    }
+
+
+    if(userObject.isUserLoggedIn()){
+        onSignIn(true);
+    }
+    else{
+        onSignIn(false);
+    }
+
+ }
+
 $.ajax({
     url: "/getdetails",
     method: "get",
@@ -53,6 +105,12 @@ $.ajax({
     })
 
     function pay(){
+        var x = document.getElementById("snackbar");
+        x.className = "show";
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    }
+
+    function checkout(){
         var x = document.getElementById("snackbar");
         x.className = "show";
         setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
