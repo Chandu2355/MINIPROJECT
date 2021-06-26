32576df9a4634fdbcd2135c4f776e96b1b3a4d17
Lalitha Soumya Ayyagari <soumyaayyagari2310@gmail.com>
2021-06-26 19:04:19 +0530

    Menu of packages changed

--- frontend/js/orders.js
+++ frontend/js/orders.js
@@ -7,32 +7,31 @@ function view()
         url: "/api/addresses",
         type: "GET",
         success: function(data) {
+           
+            if(data.length == 0)
+            {
+                
+                    var row = `<tr scope="row" >
+                        
+                    <td>You have no orders placed</td>
+                    <td></td>
+                    <td></td>
 
-        if(data.length == 0)
-        {
+                         </tr>`
             
-            var row = `<tr scope="row" >
-                   
-            <td>You have no orders placed</td>
-            <td></td>
-            <td></td>
-
-        </tr>`
-      
-        $('#tests-table').append(row)
-        }
-
-      
-        for(var i=0;i<data.length;i++) {
-            addRow(data[i]);
-
+                     $('#tests-table').append(row)
             }
-        },
-        error: function (xhr, status, error) {
-        alert(error);
-        }
-        });
+
       
+            for(var i=0;i<data.length;i++) {
+                addRow(data[i]);
+
+                }
+            },
+            error: function (xhr, status, error) {
+            alert(error);
+            }
+        });
 }
 
 function addRow(obj) {
@@ -40,6 +39,7 @@ function addRow(obj) {
     timestamp = obj._id.toString().substring(0,8)
     date = new Date( parseInt( timestamp, 16 ) * 1000 )
     date1 = date.toString().substring(4, 16)
+
     var row = `<tr scope="row" >
                    
                    <td>${obj.packageName}</td>
